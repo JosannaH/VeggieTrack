d04f2baef414cf772c41d7ad8c5239a13f9ec03f
Josanna <josanna@live.se>
2024-11-11 13:39:08 +0100

    Made changes to select-buttons

--- app/src/main/java/com/example/veggietrack/RegisterPlantsActivity.kt
+++ app/src/main/java/com/example/veggietrack/RegisterPlantsActivity.kt
@@ -1,18 +1,13 @@
 package com.example.veggietrack
 
-import android.graphics.drawable.Icon
-import android.inputmethodservice.Keyboard.Row
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -25,17 +20,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.veggietrack.ui.theme.IconDropdownMenuArrow
 import com.example.veggietrack.ui.theme.VeggieTrackTheme
 
 class RegisterPlantsActivity : ComponentActivity() {
+   // TODO localize all strings
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContent {
@@ -51,19 +43,23 @@ class RegisterPlantsActivity : ComponentActivity() {
 @Preview
 @Composable
 fun RegisterPlantsScreen() {
-   var vegetableType by remember { mutableStateOf("") }
-   var specificType by remember { mutableStateOf("") }
+
+
+   var expandedCategory by remember { mutableStateOf(false) }
+   var selectedCategory by remember { mutableStateOf("Select Category") }
+   var expandedType by remember { mutableStateOf(false) }
+   var selectedType by remember { mutableStateOf("Select Type")}
    var amount by remember { mutableStateOf("") }
    
-   val vegetableOptions = listOf("Tomato", "Carrot", "Lettuce")
-   val specificOptions = when (vegetableType) {
+   val listOfCategories = listOf("Tomato", "Carrot", "Lettuce")
+   val listOfTypes = when (selectedCategory) {
       "Tomato" -> listOf("Cherry", "Beefsteak", "Heirloom")
       "Carrot" -> listOf("Nantes", "Imperator", "Chantenay")
       "Lettuce" -> listOf("Romaine", "Iceberg", "Butterhead")
       else -> emptyList()
    }
    
-   Column(
+      Column(
       modifier = Modifier
          .fillMaxSize()
          .padding(16.dp),
@@ -73,47 +69,69 @@ fun RegisterPlantsScreen() {
          "Register your plants",
          style = MaterialTheme.typography.headlineLarge
       )
-      
       Row {
-         DropdownMenuWithTextField(
-            label = "Type of vegetable",
-            options = vegetableOptions,
-            selectedOption = vegetableType,
-            onOptionSelected = { vegetableType = it }
-         )
+         Box {
+            Button(onClick = { expandedCategory = true}) {
+               Text(text = selectedCategory)
+               Icon(
+                  imageVector = Icons.Default.ArrowDropDown,
+                  contentDescription = "Dropdown Arrow"
+               )
+            }
+            DropdownMenu(
+               expanded = expandedCategory,
+               onDismissRequest = { expandedCategory = false }) {
+               
+               listOfCategories.forEach { item ->
+                  DropdownMenuItem(
+                     text = { Text(text = item) },
+                     onClick = {
+                        selectedCategory = item
+                        selectedType = "Select Type"
+                        expandedCategory = false })
+               }
+            }
+         }
          IconButton(
-            onClick = { /*TODO*/ },
+            onClick = { /*TODO form to add new category*/ },
             content = { Icon(Icons.Default.Add, contentDescription = "Add") },
             modifier = Modifier.align(Alignment.CenterVertically)
          )
       }
-      
       Row {
-         // TODO selected option should be reset if you change the value in first dropdown
-         DropdownMenuWithTextField(
-            label = "${
-               if (vegetableType.isNotEmpty()) "Type of " + vegetableType
-               else "Specify " + "type"
-            }",
-            options = specificOptions,
-            selectedOption = specificType,
-            onOptionSelected = { specificType = it }
-         )
+         Box {
+            Button(onClick = { expandedType = true}) {
+               Text(text = selectedType)
+               Icon(
+                  imageVector = Icons.Default.ArrowDropDown,
+                  contentDescription = "Dropdown Arrow"
+               )
+            }
+            DropdownMenu(
+               expanded = expandedType,
+               onDismissRequest = { expandedType = false }) {
+               
+               listOfTypes.forEach { item ->
+                  DropdownMenuItem(
+                     text = { Text(text = item) },
+                     onClick = {
+                        selectedType = item
+                        expandedType = false })
+               }
+            }
+         }
          IconButton(
-            onClick = { /*TODO*/ },
+            onClick = { /*TODO form to add new specific type*/ },
             content = { Icon(Icons.Default.Add, contentDescription = "Add") },
-            modifier = Modifier
-               .align(Alignment.CenterVertically)
+            modifier = Modifier.align(Alignment.CenterVertically)
          )
       }
-      
       OutlinedTextField(
          value = amount,
          onValueChange = { amount = it },
-         label = { Text("Amount") },
+         label = { Text("Number of plants") },
          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
       )
-      
       Button(
          onClick = { /* TODO save registration */ },
          modifier = Modifier.align(Alignment.CenterHorizontally)
@@ -123,43 +141,8 @@ fun RegisterPlantsScreen() {
    }
 }
 
-// TODO dropdown should work when you click somewhere in the textfield
-@Composable
-fun DropdownMenuWithTextField(
-   label: String,
-   options: List<String>,
-   selectedOption: String,
-   onOptionSelected: (String) -> Unit,
-) {
-   var expanded by remember { mutableStateOf(false) }
-   val currentText = if (selectedOption.isEmpty()) label else selectedOption
-   
-   Box {
-      OutlinedTextField(
-         value = currentText,
-         onValueChange = {},
-         label = { Text(label) },
-         readOnly = true,
-         modifier = Modifier
-            //.fillMaxWidth()
-            .clickable(
-               interactionSource = remember { MutableInteractionSource() },
-               indication = null,
-            ) { expanded = true }
-      )
-      DropdownMenu(
-         expanded = expanded,
-         onDismissRequest = { expanded = false },
-      ) {
-         options.forEach { option ->
-            DropdownMenuItem(
-               text = { Text(option) },
-               onClick = {
-                  onOptionSelected(option)
-                  expanded = false
-               }
-            )
-         }
-      }
-   }
-}
+
+
+
+
+
